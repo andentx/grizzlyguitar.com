728af692a8a9e8aff19f31d458d08d771d9e35bc
Andrew D <56039221+andent2@users.noreply.github.com>
2023-04-18 09:19:41 -0400

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
       options: {
         password: process.env.SHOPIFY_SHOP_PASSWORD,
         storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+        shopifyConnections: ["collections"],
       },
     },
     "gatsby-plugin-image",
