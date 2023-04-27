7c98d2b6e7890a2151665b4fac0e4551e96b046a
Andrew D <56039221+andent2@users.noreply.github.com>
2023-04-27 16:21:37 -0400

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-image",
+    "gatsby-plugin-postcss",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-styled-components",
