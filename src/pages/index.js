b7b919e39eabf1dacd971274d00f3a3b30357e0c
Andrew D <56039221+andent2@users.noreply.github.com>
2023-05-08 11:47:52 -0400

    Reset to coming soon state

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
 import * as React from "react";
 
 import Layout from "../components/Layout";
-import TopPhoto from "../components/TopPhoto";
 
 export const Head = () => (
   <>
     <title>Grizzly Guitar</title>
-    <meta name='description' content='Description for Grizzly Guitar.' />
-    <meta name='author' content='Andrew' />
+    <meta name="description" content="Description for Grizzly Guitar." />
+    <meta name="author" content="Andrew" />
 
-    <meta name='theme-color' content='hsl(0, 0%, 0%)' />
+    <meta name="theme-color" content="hsl(0, 0%, 0%)" />
   </>
 );
 
@@ -17,11 +16,7 @@ const IndexPage = () => {
   return (
     <>
       <Layout>
-        <TopPhoto />
-        <h2>About Section</h2>
-        <h2>Services Section</h2>
-        <h2>Featured Items Section</h2>
-        <h2></h2>
+        <h2>Coming Soon</h2>
       </Layout>
     </>
   );
