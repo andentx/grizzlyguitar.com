17c18d4cff75603670d91714085cea5390a9ddb2
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-12 16:07:09 -0400

    Refactor page header logo to tailwind

--- src/components/PageHeaderLogo.js
+++ src/components/PageHeaderLogo.js
@@ -4,9 +4,16 @@ import { Link } from "gatsby";
 const PageHeaderLogo = () => {
   return (
     <>
-      <Link to="/" aria-label="home" id="headerLogo">
-        Grizzly Guitar
-      </Link>
+      <div className="flex items-center flex-grow h-12 ">
+        <Link
+          to="/"
+          aria-label="home"
+          id="headerLogo"
+          className="rounded-md focus:ring focus:ring-amber-400 focus:ring-offset-4 focus:ring-offset-gray-800 focus:outline-none "
+        >
+          Grizzly Guitar
+        </Link>
+      </div>
     </>
   );
 };
