921867c9c0855ba9c4508734d038c3120c18096f
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-12 16:15:59 -0400

    Remove global styles from layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,56 +1,8 @@
 import * as React from "react";
-
 import styled from "styled-components";
-
-import { createGlobalStyle } from "styled-components";
-
-import Color from "../components/Color";
-
 import PageHeader from "../components/PageHeader";
 import PageFooter from "../components/PageFooter";
-
 import { NavigationProvider } from "../context/navigation_context";
-
-const GlobalReset = createGlobalStyle`
- html,
- body,
- button,
- header,
- main,
- menu,
- nav,
- footer,
- section,
- article,
- pre,
- div,
- h1,
- h2,
- h3,
- h4,
- h5,
- h6,
- p,
- ol,
- ul,
- li,
- a {
-     box-sizing: border-box;
-     margin: 0;
-     padding: 0;
-     border-style: none;
-     font-style: normal;
-     font-weight: normal;
-     list-style: none;
-     text-decoration: none;
-     scroll-behavior: smooth;
- }
-
- html {
-    overflow-x: hidden;
-
-}
- `;
 
 const PageContainer = styled.div`
   min-height: 100vh;
@@ -75,8 +27,6 @@ const Layout = ({ children }) => {
   return (
     <>
       <NavigationProvider>
-        <GlobalReset />
-        <Color />
         <PageContainer>
           <PageHeader />
           <MainContent>{children}</MainContent>
