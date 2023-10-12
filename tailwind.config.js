5811d91f25ca6f162bc8443431496add35b1acf4
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-12 16:08:29 -0400

    Update tailwind extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%",
+        full: "100%",
+        1000: "1000px",
+      },
+      height: {
+        9.8: "2.4rem",
+      },
+      width: {
+        "3/10": "30%",
+        "7/10": "70%",
+      },
+      borderRadius: {
+        "5px": "5px",
+      },
+      zIndex: {
+        1000: "1000",
+      },
+      screens: {
+        "700px": "700px",
+      },
+    },
   },
   plugins: [],
 };
