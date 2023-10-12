517bcd45afcdbdf359515b8962a0d22a2903306e
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-12 13:20:46 -0400

    Remove variables from gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import "./src/styles/reset.css";
-import "./src/styles/variables.css";
 import "./src/styles/global.css";
 
 export const wrapRootElement = ({ element }) => <>{element}</>;
