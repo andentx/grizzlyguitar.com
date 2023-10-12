1e147b3f2f4085487cf152a9d421f0ac8fa8f225
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-12 16:25:43 -0400

    Add wordpress plugin to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,5 +47,11 @@ module.exports = {
       },
       __key: "images",
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: process.env.WPGRAPHQL_URL,
+      },
+    },
   ],
 };
