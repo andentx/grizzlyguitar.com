d81bb727302ae761de4bf4a0e36a08ca0f07a952
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-12 12:04:44 -0400

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,42 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from "react";
+import { Link } from "gatsby";
+import { ArrowLongLeftIcon } from "@heroicons/react/20/solid";
+import Layout from "../components/Layout";
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <>
+      <Layout>
+        <div className="grid min-h-full grid-cols-1 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr]">
+          <main className="w-full px-6 py-24 mx-auto max-w-7xl sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
+            <div className="max-w-lg">
+              <p className="text-base font-semibold leading-8 text-gray-600">
+                404
+              </p>
+              <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                Page not found
+              </h1>
+              <p className="mt-6 text-base leading-7 text-gray-600">
+                Sorry, we couldn't find the page you're looking for.
+              </p>
+              <div className="mt-10">
+                <Link
+                  to="/"
+                  className="inline-flex items-center p-2 mb-8 text-sm font-semibold leading-7 text-gray-600 transition-colors ease-in-out rounded-md hover:text-black focus:outline-none focus:ring focus:ring-amber-400"
+                >
+                  <ArrowLongLeftIcon
+                    className="w-5 h-5 mr-2"
+                    aria-hidden="true"
+                  />
+                  <span>Back to home</span>
+                </Link>
+              </div>
+            </div>
+          </main>
+        </div>
+      </Layout>
+    </>
+  );
+};
 
-export default NotFoundPage
-
-export const Head = () => <title>Not found</title>
+export default NotFoundPage;
