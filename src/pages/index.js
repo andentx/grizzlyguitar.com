ced70d59456e4c7752b60321ba4ec5e141267533
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-12 12:02:29 -0400

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import Layout from "../components/Layout";
 
 export const Head = () => (
@@ -16,7 +15,9 @@ const IndexPage = () => {
   return (
     <>
       <Layout>
-        <h2>Coming Soon</h2>
+        <div className="w-full bg-white">
+          <h2>Coming Soon</h2>
+        </div>
       </Layout>
     </>
   );
