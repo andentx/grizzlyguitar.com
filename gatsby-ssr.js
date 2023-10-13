27db85b4797dcc27bc665a2fac3140562990bc6e
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-13 13:33:43 -0400

    Add products filters context to gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,15 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: "en" })
-}
+import React from "react";
+import { ProductsProvider } from "./src/context/products_context";
+import { ProductsFiltersProvider } from "./src/context/products_filters_context";
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ProductsProvider>
+      <ProductsFiltersProvider>{element}</ProductsFiltersProvider>
+    </ProductsProvider>
+  );
+};
+
+export const onRenderBody = ({ setHtmlAttributes }) => {
+  setHtmlAttributes({ lang: "en" });
+};
