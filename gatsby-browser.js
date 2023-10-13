fedd38fa2bf98b79e20a7bde29147b2a12c13942
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-13 14:04:17 -0400

    Implement cart context

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { ProductsProvider } from "./src/context/products_context";
 import { ProductsFiltersProvider } from "./src/context/products_filters_context";
+import { CartProvider } from "./src/context/cart_context";
 
 import "./src/styles/reset.css";
 import "./src/styles/global.css";
@@ -8,7 +9,9 @@ import "./src/styles/global.css";
 export const wrapRootElement = ({ element }) => {
   return (
     <ProductsProvider>
-      <ProductsFiltersProvider>{element}</ProductsFiltersProvider>
+      <ProductsFiltersProvider>
+        <CartProvider>{element}</CartProvider>
+      </ProductsFiltersProvider>
     </ProductsProvider>
   );
 };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { ProductsProvider } from "./src/context/products_context";
 import { ProductsFiltersProvider } from "./src/context/products_filters_context";
+import { CartProvider } from "./src/context/cart_context";
 
 export const wrapRootElement = ({ element }) => {
   return (
     <ProductsProvider>
-      <ProductsFiltersProvider>{element}</ProductsFiltersProvider>
+      <ProductsFiltersProvider>
+        <CartProvider>{element}</CartProvider>
+      </ProductsFiltersProvider>
     </ProductsProvider>
   );
 };
