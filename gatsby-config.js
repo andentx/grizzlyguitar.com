1a24f8bc53dd55c5b356d8166d7f136f7a5f0336
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-13 15:02:16 -0400

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 module.exports = {
   siteMetadata: {
     title: `Grizzly Guitar`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.grizzlyguitar.com`,
   },
   plugins: [
     {
@@ -22,20 +22,14 @@ module.exports = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-sharp",
     "gatsby-plugin-tailwindcss",
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-transformer-sharp",
       options: {
         quality: 90,
       },
     },
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
@@ -65,6 +59,26 @@ module.exports = {
         dataset: process.env.SANITY_DATASET,
         token: process.env.SANITY_TOKEN,
         graphqlTag: "default",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Grizzly Guitar",
+        short_name: "Grizzly Guitar",
+        background_color: "rgb(31, 41, 55)",
+        theme_color: "rgb(31, 41, 55)",
+        cache_busting_mode: "query",
+        crossOrigin: `use-credentials`,
+        display: "browser",
+        icon: "src/images/favicons/icon.png",
+        start_url: "/",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-html-attributes",
+      options: {
+        lang: "en-US",
       },
     },
   ],
