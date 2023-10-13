2cb7f339f6b523ea4c1c48a03b0282704e7efee0
Andrew D <56039221+andent2@users.noreply.github.com>
2023-10-13 13:33:57 -0400

    Add products filters context to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,14 @@
 import * as React from "react";
+import { ProductsProvider } from "./src/context/products_context";
+import { ProductsFiltersProvider } from "./src/context/products_filters_context";
+
 import "./src/styles/reset.css";
 import "./src/styles/global.css";
 
-export const wrapRootElement = ({ element }) => <>{element}</>;
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ProductsProvider>
+      <ProductsFiltersProvider>{element}</ProductsFiltersProvider>
+    </ProductsProvider>
+  );
+};
